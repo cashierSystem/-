d712a8d1f3562e2796f3acdeae93587feb2d4bcf
zhaosongyan <741875758@qq.com>
2023-03-18 14:27:25 +0800

    init 项目

---
+++ src/main/java/com/yike/ykoperationservice/YkOperationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yike.ykoperationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YkOperationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YkOperationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yike/ykoperationservice/YkOperationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yike.ykoperationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YkOperationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
